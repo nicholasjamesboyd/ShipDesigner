0ea54b8552d00b0ac7ad385ce6fd2d1867315efe
nicholasjamesboyd <47800967+nicholasjamesboyd@users.noreply.github.com>
2020-05-27 20:38:35 -0400

    Added in more motion functions

--- MotionFunctions.py
+++ MotionFunctions.py
@@ -4,23 +4,24 @@ def CalculateMotions(hull_type, Length, Beam, Draft, Seas, MaxVelocity):
     if (hull_type == "Axe"):
         Motions = (0.24 - 0.0080*Length - 0.025*Draft + 0.12*Seas[:,0] + 0.11*Seas[:,1] - 0.00043*Length*Seas[:,0] + 0.0024*Draft*Seas[:,1] + 0.0075*Seas[:,0]*Seas[:,1] + 0.000032*Length**2 - 0.0071*Seas[:,0]**2 - 0.0063*Seas[:,1]**2)**2
         DownTime = np.size(Motions[Motions>MaxVelocity],0)/np.size(Seas,0)
-        DownTime = round(Downtime,3)
+        DownTime = round(DownTime,3)
         SailingIndices = Motions < MaxVelocity
         SailingConditions = Seas[SailingIndices,:]
         return DownTime, SailingConditions
 
     elif (hull_type == "X"):
-        Motions = 0.0 #TODO Define
+        Motions = (-1.20 - 0.0036*Length + 0.093*Seas[:,0] + 0.68*Seas[:,1] - 0.002 * Length * Seas[:,1] + 0.0068 * Seas[:,0] * Seas[:,1] + 0.000036 * Length**2 - 0.0073*Seas[:,0]**2 - 0.064 * Seas[:,1]**2 + 0.00012 * Length * Seas[:,1]**2 + 0.0018 * Seas[:,1]**3)**2
         DownTime = np.size(Motions[Motions>MaxVelocity],0)/np.size(Seas,0)
-        DownTime = round(Downtime,3)        
+        DownTime = round(DownTime,3)
         SailingIndices = Motions < MaxVelocity
         SailingConditions = Seas[SailingIndices,:]
         return DownTime, SailingConditions
 
     elif (hull_type == "Vertical"):
-        Motions = 0.0 #TODO Define
+        Motions = (-1.11 + 0.0035 * Length + 0.031 * Seas[:,0] + 0.58 * Seas[:,1] - 0.0022 * Length * Seas[:,1] + 0.0072 * Seas[:,0] * Seas[:,1] - 0.053 * Seas[:,1]**2 + 0.00014 * Length * Seas[:,1]**2 + 0.0014 * Seas[:,1]**3)**2
         DownTime = np.size(Motions[Motions>MaxVelocity],0)/np.size(Seas,0)
-        DownTime = round(Downtime,3)        
+        DownTime = round(DownTime,3)
+        print(DownTime)
         SailingIndices = Motions < MaxVelocity
         SailingConditions = Seas[SailingIndices,:]
         return DownTime, SailingConditions
@@ -28,7 +29,7 @@ def CalculateMotions(hull_type, Length, Beam, Draft, Seas, MaxVelocity):
     elif (hull_type == "Bulbous"):
         Motions = 0.0 #TODO Define
         DownTime = np.size(Motions[Motions>MaxVelocity],0)/np.size(Seas,0)
-        DownTime = round(Downtime,3)        
+        DownTime = round(DownTime,3)       
         SailingIndices = Motions < MaxVelocity
         SailingConditions = Seas[SailingIndices,:]
         return DownTime, SailingConditions
